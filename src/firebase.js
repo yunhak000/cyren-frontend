eab2832e037beff29b9a89fef2519d0f8d5b3d90
이윤학 <yunhak@iyunhag-ui-MacBookPro.local>
2022-07-17 18:13:30 +0900

    [REFACTOR] web push alert 기능 aws 오류로 인한 revert

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,19 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const messaging = getMessaging(app);
-
 export const auth = getAuth(app);
 
 export const provider = new GoogleAuthProvider();
-
-export const getTokenValue = async (setTokenFound) => {
-  try {
-    const currentToken = await getToken(messaging, { vapidKey: process.env.REACT_APP_VAPID_KEY });
-    if (currentToken) {
-      setTokenFound(currentToken);
-    }
-  } catch (err) {
-    console.log("An error occurred while retrieving token. ", err);
-  }
-};
